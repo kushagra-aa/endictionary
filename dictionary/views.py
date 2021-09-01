1c8a77d45d4977a12b22725f05558f851db2de4b
kushagra-aa <kushagraagnihotri24@gmail.com>
2021-09-01 15:05:54 +0530

    initial commit

---
+++ dictionary/views.py
@@ -0,0 +1,27 @@
+from django.http import request
+from django.shortcuts import render
+from PyDictionary import PyDictionary
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+# The PyDictionary.synonym function tries to look up synonyms on thesaurus.com, but the code is out of date. It's looking for html structures that don't exist anymore.
+
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonyms = dictionary.synonym(search)
+    antonyms = dictionary.antonym(search)
+    print(antonyms)
+    print(dictionary.synonym(search))
+    context = {
+        'meaning': meaning['Noun'][0],
+        'synonyms': synonyms,
+        'antonyms': antonyms
+    }
+    return render(request, 'word.html', context)
